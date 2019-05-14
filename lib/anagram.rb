c69eb11f657ce480769e0a8a24fe1360b0b1fac6
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-05-14 04:11:48 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word 
+  
+  def initialize(some_word)
+    @word = some_word
+  end 
+  
+  def match(array_of_words)
+    match_array = []
+    #array_of_words = string.split (" ")
+    array_of_words.each do |some_word|
+      if @word.chars.sort.join == some_word.chars.sort.join
+        match_array << some_word
+      end 
+    end
+    return match_array
+  end 
+  
+end #Anagram
