c403a7161a8f9836edc010167c101cf680bf0421
Joanna Yuhas <joannayuhas@gmail.com>
2019-05-14 19:57:56 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+class Anagram
+attr_accessor :word
+
+def initialize(word)
+  @word = word
+end
+
+def match(array)
+  matches = []
+  array.each do |comp|
+    if comp.split("").sort == @word.split("").sort
+      matches << comp
+    end
+  end
+  matches
+end
+
+end
