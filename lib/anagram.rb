f073e7aee821b0d8a8e18cb40234940e2bdb7e72
Cody Frank <cody.frank30@gmail.com>
2019-05-19 15:52:14 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
 # Your code goes here!
+require "pry"
+class Anagram
+  attr_reader :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    new_array = []
+    word_a = @word.split(//).sort
+    array.each do |x|
+      comp_word = x.split(//).sort
+      if word_a == comp_word
+        new_array << x
+      end
+    end
+    new_array
+  end
+
+end
