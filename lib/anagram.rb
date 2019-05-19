b8c01db3f8032471513de8c58b1cf7b761bf9a51
Caleb Aviles <earthctzn1@gmail.com>
2019-05-19 16:59:19 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -4,16 +4,22 @@ class Anagram
   
   def initialize(word)
     @word = word.split("")
-   
   end
   
+#longer version of .match
   def match(array)
-   ary = []
-   array.each do |w| 
+  ary = []
+  array.each do |w| 
       if w.split("").sort == @word.sort
       ary << w
       end  
     end
     ary
   end 
+# Short version of .match
+  
+  def match(array)
+    array.select{|word|word.split("").sort == @word.sort}
+  end
+
 end 
