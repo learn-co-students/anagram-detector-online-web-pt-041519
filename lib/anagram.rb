2ca70a3c4ed837e5a30ae3dc9bcf771ca1c9b5f3
Lorena C <lorena@eunknown.org>
2019-05-20 02:01:01 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(equal)
+    equal.select do |wordy|
+      (@word.split("").sort) == (wordy.split("").sort)
+    end
+  end
+
+end
