5fc374de38a1c702b254845d644b517c9394c9e1
Cody Alvarez <jedi.alvarez805@gmail.com>
2019-07-01 15:09:42 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
-# Your code goes here!
+class Anagram
+    attr_accessor :name
+
+
+def initialize(word)
+    @name = name
+end
+
+def match(words)
+    words.select { |word| word.split("").sort == @name.split("").sort }
+    
+
+end
+end
